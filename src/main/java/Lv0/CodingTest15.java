71a67f8dbd328c5b4e0391fa2b5eab862774721f
hesong <java.she123@gmail.com>
2023-11-01 14:06:29 +0900

    [Day3] 두 수의 연산값 비교하기

---
+++ src/main/java/Lv0/CodingTest15.java
@@ -0,0 +1,29 @@
+package Lv0;
+
+// [Day3] 두 수의 연산값 비교하기
+class Compare2 {
+    public int compareCalculation(int a, int b) {
+        int answer = 0;
+        String num = "" + a + b;
+        int val1 = Integer.valueOf(num);
+        int val2 = 2 * a * b;
+
+        // 방법1.
+        answer = val1 > val2 ? val1 : val2;
+
+        // 방법2.
+//        answer = Math.max(val1, val2);
+
+        return answer;
+    }
+}
+
+public class CodingTest15 {
+    public static void main(String[] args) {
+        Compare2 c = new Compare2();
+        int a = 10;
+        int b = 2;
+        int result = c.compareCalculation(a, b);
+        System.out.println(result);
+    }
+}
